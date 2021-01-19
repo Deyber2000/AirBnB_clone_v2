4e84b9cbaa0d96f3f7c0331584b5b8a095075ffb
Deyber2000 <garzondeiber6@gmail.com>
2021-01-19 18:42:23 -0500

    checking loop

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -18,8 +18,7 @@ def cities_by_states():
 
     States/cities are sorted by name.
     """
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
